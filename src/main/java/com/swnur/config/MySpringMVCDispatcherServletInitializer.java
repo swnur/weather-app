938ea533f0ce0b12fdf13e6045cb15710b358151
Nurbek Shukan <shukannurbek@gmail.com>
2025-06-04 11:19:54 +0200

    Configure the Spring web MVC project

---
+++ src/main/java/com/swnur/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.swnur.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
